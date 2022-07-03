bc21e23d2c563c851d849aa3772d09a35b81bab2
Vincent Milum Jr <git@darkain.com>
2022-07-03 12:42:43 -0700

    Update vince.sh

--- vince.sh
+++ vince.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-fetch -qo - https://raw.githubusercontent.com/darkain/FreeBSD-Server/master/bootstrap.sh | sh
+fetch -qo - https://raw.githubusercontent.com/darkain/FreeBSD-Server/main/bootstrap.sh | sh
